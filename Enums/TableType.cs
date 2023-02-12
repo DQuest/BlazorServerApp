fe72486f420062f9258ac766fe91da634457cd2a
DQuest <DQuest1996@gmail.com>
2023-02-12 20:08:17 +0500

    Init

---
+++ Enums/TableType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace BlazorServerApp.Enums;
+
+public enum TableType
+{
+    [Description("Животные")]
+    Animal,
+    
+    [Description("Города")]
+    City,
+    
+    [Description("Пользователи")]
+    User
+}
